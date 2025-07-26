b5924acb406363bb362b37055eb876934b8b4dc6
Swarupanand Popat Bajbalkar <151636990+Swarupanand05@users.noreply.github.com>
2025-07-26 21:24:52 +0530

    Add files via upload

---
+++ Circular_linked_list.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int data;
+	struct node *next;
+};
+struct node *head;
+void create()
+{
+	struct node *tmp,*newnode;
+	head=NULL;
+	int choice=1;
+	while(choice)
+	{
+		newnode=(struct node*)malloc(sizeof(struct node));
+		printf("enter data:");
+		scanf("%d",&newnode->data);
+		newnode->next=NULL;
+		if(head==NULL)
+		{
+			head=tmp=newnode;
+		}
+		else
+		{
+			tmp->next=newnode;
+			tmp=newnode;
+		}
+		tmp->next=head;
+		printf("want to store a data(0/1)");
+		scanf("%d",&choice);
+	}
+}
+void display()
+{
+	struct node *tmp;
+	if(head==NULL)
+	{
+		printf("empty list");
+	}
+	else
+	{
+		tmp=head;
+		while(tmp->next!=head)
+		{
+			printf("%d",tmp->data);
+			tmp=tmp->next;
+		}
+		printf("%d\n",tmp->data);
+	}
+	
+	
+}
+int main()
+{
+	
+	create();
+	display();
+	
+}
+
